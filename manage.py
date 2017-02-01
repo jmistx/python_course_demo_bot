ff0df573caeab0a60b50b9b45fa9f5e30d71d393
Oleg Blokhin <jmistx@users.noreply.github.com>
2017-02-01 18:13:30 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,17 @@
-print('Hello world')
+import os
+from telegram.ext import Updater, MessageHandler, Filters
+
+TOKEN = os.environ.get('TOKEN', 'TOKEN')
+PORT = os.onviron.get('PORT', 'PORT')
+
+
+def echo(bot, update):
+  update.message.reply_text('Bot answer: ' + update.message.text)
+
+updater = Updater(TOKEN)
+updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.setWebhook('https://calm-crag-96590.herokuapp.com/{0}'.format(TOKEN))
+updater.idle()
